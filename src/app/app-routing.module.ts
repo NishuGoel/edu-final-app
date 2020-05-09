8a63ea2e668333c8287e27939f9ac87d95285524
Nishu goel <nishugoel@Nishus-MacBook-Air.local>
2020-05-09 16:20:15 +0530

    routes add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductCardComponent } from './product/product-card/product-card.component';
+import { CartComponent } from './cart/components/cart.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 
 export const routes: Routes = [
-  // router states here
+  {path: 'home', component: HomeComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: 'product/:id', component: ProductCardComponent},
+  {path: 'checkout', component: CartComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', component: ErrorPageComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    HomeComponent
+    HomeComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
